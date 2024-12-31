ef00e5ff44810e903388976a7995b41dcc3c4cd3
Akin Wilson <97032232+akinwilson@users.noreply.github.com>
2024-12-31 16:39:59 +0000

    Create install_nividia_container_toolkit.sh
    add script to install nvidia container toolkit. allow for containerized training with GPU access.

---
+++ configs/install_nividia_container_toolkit.sh
@@ -0,0 +1,19 @@
+#!/bin/bash 
+# source: https://docs.nvidia.com/datacenter/cloud-native/container-toolkit/latest/install-guide.html
+set -e 
+
+echo "Configuring the production repository ..."
+curl -fsSL https://nvidia.github.io/libnvidia-container/gpgkey | sudo gpg --dearmor -o /usr/share/keyrings/nvidia-container-toolkit-keyring.gpg \
+  && curl -s -L https://nvidia.github.io/libnvidia-container/stable/deb/nvidia-container-toolkit.list | \
+    sed 's#deb https://#deb [signed-by=/usr/share/keyrings/nvidia-container-toolkit-keyring.gpg] https://#g' | \
+    sudo tee /etc/apt/sources.list.d/nvidia-container-toolkit.list
+
+echo "Updating apt repository ..."
+sudo apt-get update
+
+echo "Installing container nvidia toolkit ..."
+sudo apt-get install -y nvidia-container-toolkit
+
+echo "Restarting docker daemon ..."
+systemctl restart docker
+
